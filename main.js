8617034c2193292c325b8a49aaeb7aa02c967689
Michael F <mfriedmanzz01@gmail.com>
2015-01-28 12:48:25 -0700

    update

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 $(document).on('ready', function() {
-  	$(document).on('click', function(event) {
+  	$(document).on('dblclick', function(event) {
 
   		var x = event.pageX;
   		var y = event.pageY;
   		//$(this).append('<p> Here we are</p>');   
   		var marker = $("<div class='mapclass'>You are here</div>");
-
-
+      var noteText = "";
+      var noteBox ;
+      var markerText = $("<div class='notes'>enter notes here: <input type='text'> <input type = 'submit'></div>");
+      marker.append(markerText);
+      markerText.find("[type=submit]").on('click', function () {
+        noteText = markerText.find("[type=text]").val();
+        console.log("We got here " + noteText);
+        noteBox = $("<div class = 'noteBox'> noteText</div>");
+        console.log("textbox ", noteBox);
+        $(".container").append(noteBox);
+      })
+        
+    
+      //console.log("this us", noteText);
   		marker.css({position: "absolute", top: y, left: x});
 
   		$(".container").append(marker);
+      
 
-  		$(document).on("click", ".mapclass", function() {
+  		$(document).on("dblclick", ".mapclass", function() {
   				$('.mapclass').remove();
   				event.stopProgration();
 
 
   		});
+
 
   		// $(this).append('id', 'div').addClass('pointer').css({
   		// 	top: x,
