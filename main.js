86a98ca09801a10ed4dd77c6724f025812f58959
Michael F <mfriedmanzz01@gmail.com>
2015-01-28 14:53:22 -0700

    mm

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
 $(document).on('ready', function() {
-  	$(document).on('dblclick', function(event) {
+		$(document).on("dblclick", ".mapclass", function() {
+				$('.mapclass').remove();
+				event.stopProgration();
 
-  		var x = event.pageX;
-  		var y = event.pageY;
-  		//$(this).append('<p> Here we are</p>');   
-  		var marker = $("<div class='mapclass'>You are here</div>");
+
+		});
+
+    $(document).on('dblclick', function(event) {
+
+      var x = event.pageX;
+      var y = event.pageY;
+      //$(this).append('<p> Here we are</p>');   
+      var marker = $("<div class='mapclass'>You are here</div>");
       var noteText = "";
-      var noteBox ;
+      //var noteBox ;
       var markerText = $("<div class='notes'>enter notes here: <input type='text'> <input type = 'submit'></div>");
       marker.append(markerText);
       markerText.find("[type=submit]").on('click', function () {
         noteText = markerText.find("[type=text]").val();
         console.log("We got here " + noteText);
-        noteBox = $("<div class = 'noteBox'> noteText</div>");
+        var noteBox = $("<div class = 'noteBox'> " + noteText + "</div>");
         console.log("textbox ", noteBox);
-        $(".container").append(noteBox);
+        marker.append(noteBox);
+        console.log()
       })
         
     
       //console.log("this us", noteText);
-  		marker.css({position: "absolute", top: y, left: x});
+      marker.css({position: "absolute", top: y, left: x});
 
-  		$(".container").append(marker);
-      
+      $(".container").append(marker);
 
-  		$(document).on("dblclick", ".mapclass", function() {
-  				$('.mapclass').remove();
-  				event.stopProgration();
-
-
-  		});
 
 
   		// $(this).append('id', 'div').addClass('pointer').css({
